3b34ae0f437622be73e05ea76d38eccdb865ede1
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-04-15 20:49:48 -0300

    feat: created taks.js

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -17,46 +17,63 @@ window.onload = function(){
     fetch(url, settings)
     .then(response => response.json())
     .then(json => {
-       
-        for(let i=0; i<json.length; i++){
-            document.getElementsByClassName("tarefa").innerHTML = `
-            <div class="not-done"></div>
-            <div class="descricao">
-              <p class="nome">Nova tarefa</p>
-              <p class="timestamp">Criada em: 15/07/21</p>
-            </div>
-          
+        let schemaA = document.getElementById("skeleton");
+        let schemaB = document.getElementById("closed");
+        json.forEach(e => {
+            if(e.completed == false){
+                schemaA.innerHTML += `
+                    <li class="tarefa">
+                        <div class="not-done"></div>
+                        <div class="descricao">
+                            <p class="nome">${e.description}</p>
+                            <p class="timestamp">Criada em: ${e.createdAt}</p>
+                        </div>
+                    </li>
             `
-        }
+            }else{
+                schemaB.innerHTML += `
+                    <li class="tarefa">
+                    <div class="not-done"></div>
+                    <div class="descricao">
+                        <p class="nome">${e.description}</p>
+                        <p class="timestamp">Criada em: ${e.createdAt}</p>
+                    </div>
+                    </li>
+                `
+            }
+           
+        });
     })
     .catch(err => console.error(err))
 }
 
     
+function createTask(){
+    let task = document.getElementById("novaTarea").value;
+    let token = localStorage.getItem('token');
+    let url = `https://ctd-todo-api.herokuapp.com/v1/tasks`;
+    
+    let settings = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": token,
+        },
+        body: JSON.stringify({
+            "description": task,
+            "completed": false,
+        })
+    }
+
+
+    fetch(url, settings)
+        .then(response => response.json())
+        .then(json => {
+            console.log(json)
+        })
+        .catch(err => console.error(err));
+}
 
 addEventListener("submit", e => {
-        let task = document.getElementById("novaTarea").value;
-        let token = localStorage.getItem('token');
-        let url = `https://ctd-todo-api.herokuapp.com/v1/tasks`;
-        
-        let settings = {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": token,
-            },
-            body: JSON.stringify({
-                "description": task,
-                "completed": false,
-            })
-        }
-    
-    
-        fetch(url, settings)
-            .then(response => response.json())
-            .then(json => {
-                console.log(json)
-            })
-            .catch(err => console.error(err));
-    
+    e.preventDefault();
 })
